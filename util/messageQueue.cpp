ff17f68f1a4239181f0f7cfc8d809a918e20f3cc
Xiong Jun <adairjun@gmail.com>
2015-12-22 00:13:10 +0800

    ADD: add temp messageQueue

---
+++ util/messageQueue.cpp
@@ -0,0 +1,40 @@
+#include "MQPool/messageQueue.h"
+#include "MQPool/parse_xml.h"
+#include "MQPool/parse_json.h"
+#include <sys/ipc.h>
+
+MessageQueue::MessageQueue()
+    : msgFile_("key"){
+	key_t key = ftok(msgFile_,'a');
+	msgid_ = msgget(key,S_IRUSR|S_IWUSR|IPC_CREAT|IPC_EXCL)
+}
+
+MessageQueue::MessageQueue(string msgFile)
+    : msgFile_(msgFile) {
+	key_t key = ftok(msgFile_,'a');
+	msgid_ = msgget(key,S_IRUSR|S_IWUSR|IPC_CREAT|IPC_EXCL)
+}
+
+MessageQueue::~MessageQueue() {
+
+}
+
+void MessageQueue::Dump() const {
+  printf("\n=====MessageQueue Dump START ========== \n");
+
+  printf("\n=====MessageQueue DUMP END ============\n");
+}
+
+int MessageQueue::GetMsgID() const {
+  return msgid_;
+}
+
+string MessageQueue::GetMsgFile() const {
+  return msgFile_;
+}
+
+void MessageQueue::SetMsgFile(const string& msgFile) {
+  msgFile_ = msgFile;
+  key_t key = ftok(msgFile_,'a');
+  msgid_ = msgget(key,S_IRUSR|S_IWUSR|IPC_CREAT|IPC_EXCL)
+}
